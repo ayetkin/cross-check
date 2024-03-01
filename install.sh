889a8be0669ecca7b07f981210f092eef6222ea0
Ramazan Sancar <ramazansancar4545@gmail.com>
2024-03-01 20:52:58 +0000

    upd: Fix formatting in install.sh

--- install.sh
+++ install.sh
@@ -66,25 +66,25 @@ install () {
   systemctl start cross-check
   # shellcheck disable=SC2181
   if [ $? -ne 0 ]; then
-      echo "Error while starting service"
-      exit 1
+    echo "Error while starting service"
+    exit 1
   fi
   echo -e ":::::::: Installing done ::::::::"
 }
 
 spinner()
 {
-    local pid=$!
-    local delay=0.75
-    local spinstr='|/-\'
-    while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
-        local temp=${spinstr#?}
-        printf " [%c]  " "$spinstr"
-        local spinstr=$temp${spinstr%"$temp"}
-        sleep $delay
-        printf "\b\b\b\b\b\b"
-    done
-    printf "    \b\b\b\b"
+  local pid=$!
+  local delay=0.75
+  local spinstr='|/-\'
+  while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+    local temp=${spinstr#?}
+    printf " [%c]  " "$spinstr"
+    local spinstr=$temp${spinstr%"$temp"}
+    sleep $delay
+    printf "\b\b\b\b\b\b"
+  done
+  printf "    \b\b\b\b"
 }
 
 #Copy service config and application
